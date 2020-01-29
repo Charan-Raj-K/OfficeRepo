74b48f1884d1a2f5bc9c5a8730a48bc8bd31261d
Charan-Raj-K <40881285+Charan-Raj-K@users.noreply.github.com>
2020-01-29 11:28:23 +0530

    Update Hello World.java

--- Hello World.java
+++ Hello World.java
@@ -4,9 +4,9 @@ public static void main(String args[])
 {
 System.out.println("Hello World");
   
-System.out.println("2nd Commit.");
+System.out.println("2nd Commit");
 
-System.out.println("3rd Commit.");
+System.out.println("3rd Commit");
 }
 
 }
